c59623e9faaf28b523340028b29430ccae82c140
Mihai Dita <mihai.dita@metrosystems.net>
2019-10-04 15:43:59 +0300

    fix lint errors

--- build/utils/loadData.js
+++ build/utils/loadData.js
@@ -2,27 +2,27 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const hash = require("object-hash");
 function initialData() {
-    const metadata_1 = hash({ key1: 'value1' });
-    const metadata_2 = hash({ key2: 'value2' });
-    const metadata_3 = hash({ key3: 'value3' });
+    const metadata1 = hash({ key1: 'value1' });
+    const metadata2 = hash({ key2: 'value2' });
+    const metadata3 = hash({ key3: 'value3' });
     return [
         {
             type: 'normal',
             serial: '123-456-789',
             color: 'red',
-            metadata: metadata_1
+            metadata: metadata1
         },
         {
             type: 'smartphone',
             serial: '456-123-789',
             color: 'blue',
-            metadata: metadata_2
+            metadata: metadata2
         },
         {
             type: 'smartphone',
             serial: '789-456-789',
             color: 'green',
-            metadata: metadata_3
+            metadata: metadata3
         }
     ];
 }
