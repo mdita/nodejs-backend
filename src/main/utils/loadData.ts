63eecaf356ccacada16d6ace31e909beabf8bd50
Mihai Dita <mihai.dita@metrosystems.net>
2019-10-04 15:42:03 +0300

    metadata should be a hashed json object

--- src/main/utils/loadData.ts
+++ src/main/utils/loadData.ts
@@ -1,24 +1,28 @@
 import { PhoneDetails } from '../repository/PhoneRepository'
+import * as hash from 'object-hash';
 
 export function initialData(): PhoneDetails[] {
+  const metadata_1 = hash({key1: 'value1'});
+  const metadata_2 = hash({key2: 'value2'});
+  const metadata_3 = hash({key3: 'value3'});
   return [
     {
       type: 'normal',
       serial: '123-456-789',
       color: 'red',
-      metadata: 'some metadata'
+      metadata: metadata_1
     },
     {
       type: 'smartphone',
       serial: '456-123-789',
       color: 'blue',
-      metadata: 'some metadata2'
+      metadata: metadata_2
     },
     {
       type: 'smartphone',
       serial: '789-456-789',
       color: 'green',
-      metadata: 'some metadata3'
+      metadata: metadata_3
     }
   ]
 }
